7ad65590f29b57e605ea4f3c2f5c8da27f3a0779
RyodoTanaka <groadpg@gmail.com>
2018-07-04 13:57:34 +0900

    [add] docker install script

---
+++ docker_install.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yes | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
