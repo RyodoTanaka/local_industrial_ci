64d4e20c9280ebd52863bfe3d4540daf00dcfd22
RyodoTanaka <groadpg@gmail.com>
2018-07-09 02:24:40 +0900

    [fix] compile in correct path

--- install_industrial_ci.bash
+++ install_industrial_ci.bash
@@ -3,6 +3,7 @@
 SCRIPT_PATH=$( dirname ${BASH_SOURCE:-$0} )
 CURRENT=$(pwd)
 
+cd ${SCRIPT_PATH}
 git clone https://github.com/ros-industrial/industrial_ci.git
 catkin build
 source devel/setup.bash
