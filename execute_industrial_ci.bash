30d49dc700e0d30c43b56a10984d0532ce054d9c
RyodoTanaka <groadpg@gmail.com>
2018-07-09 02:25:12 +0900

    [update] add ROS_REPO

--- execute_industrial_ci.bash
+++ execute_industrial_ci.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 -e ]; then
-    rosrun industrial_ci run_ci ROS_DISTRO="kinetic" UPSTREAM_WORKSPACE=file ROSINSTALL_FILENAME=$1
+    rosrun industrial_ci run_ci ROS_DISTRO="kinetic" ROS_REPO=ros-shadow-fixed UPSTREAM_WORKSPACE=file ROSINSTALL_FILENAME=$1
 else
-    rosrun industrial_ci run_ci ROS_DISTRO="kinetic"
+    rosrun industrial_ci run_ci ROS_DISTRO="kinetic" ROS_REPO=ros-shadow-fixed
 fi
